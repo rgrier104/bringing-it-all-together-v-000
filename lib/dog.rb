e0231190bcacddce51a6a6a88338529c6f405a0c
Rachel Grier <rgrier104@gmail.com>
2019-01-13 01:00:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,4 +9,16 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
 end
